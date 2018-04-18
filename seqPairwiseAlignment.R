323202348368ca7cd145a2a8414f4b06380479fa
rumarova <38050024+rumarova@users.noreply.github.com>
2018-04-18 11:33:28 -0600

    Add files via upload

---
+++ seqPairwiseAlignment.R
@@ -0,0 +1,52 @@
+#import Biostrings and seqinr libraries
+library(Biostrings)
+library (seqinr)
+library(tools)
+
+# reading in seq files into a seq vector
+f <- length(list.files(pattern = "*.txt"))
+seqs = c()
+
+for (i in list.files(pattern = "*.txt")){
+  s <-read.fasta(file=i, seqtype="AA", as.string = TRUE, seqonly = TRUE)
+  seqs <- c(seqs, s)
+}
+
+# parsing proteins IDs to name rows and cols of a matrix
+IDs = c()
+for (i in list.files(pattern = "*.txt")){
+  n <-file_path_sans_ext(i)
+  IDs <- c(IDs, n)
+}
+
+
+
+score.psa = c()
+data("BLOSUM62")
+for (j in 1:length(seqs)){
+  psa <- pairwiseAlignment(seqs[[1]], seqs[[j]], type = "local", substitutionMatrix = PAM30, gapOpening = 10, gapExtension = 0.5, scoreOnly = TRUE)
+  score.psa <- c(score.psa, psa)
+}
+
+targetPairSeqAlign <- matrix(score.psa, nrow = 1, ncol = f)
+
+for (i in 2:length(seqs)) {
+  score.psa = c()
+  for (j in 1:length(seqs)){
+    psa <- pairwiseAlignment(seqs[[i]], seqs[[j]], type = "local", substitutionMatrix = BLOSUM62, gapOpening = 10, gapExtension = 0.5, scoreOnly = TRUE)
+    score.psa <- c(score.psa, psa)
+  }
+  targetPairSeqAlign <- rbind(targetPairSeqAlign, score.psa)
+ 
+}
+
+# Name columns and rows of the matrix
+colnames(targetPairSeqAlign) <- IDs
+rownames(targetPairSeqAlign) <- IDs
+
+write.csv(targetPairSeqAlign, file = "targetPairwiseAlign.csv")
+
+
+
+
+
